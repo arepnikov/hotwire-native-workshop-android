37c6d2bb72a0f7e90e22b313b8a7d09708a8fedd
Alexander Repnikov <a.repnikov@visuality.pl>
2026-03-03 18:57:06 +0100

    Base Android app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("dev.hotwire:core:1.2.6")
+    implementation("dev.hotwire:navigation-fragments:1.2.6")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/hotwirenativeworkshop/MainActivity.kt
+++ app/src/main/java/com/example/hotwirenativeworkshop/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.hotwirenativeworkshop
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import dev.hotwire.navigation.activities.HotwireActivity
+import dev.hotwire.navigation.navigator.NavigatorConfiguration
+import dev.hotwire.navigation.util.applyDefaultImeWindowInsets
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : HotwireActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        findViewById<View>(R.id.main_nav_host).applyDefaultImeWindowInsets()
     }
+
+    override fun navigatorConfigurations() = listOf(
+        NavigatorConfiguration(
+            name = "main",
+            startLocation = "https://hotwire-native-demo.dev",
+            navigatorHostId = R.id.main_nav_host
+        )
+    )
 }
