2c15200ff7c64241aed7c62c8bee86db6d535cd7
Alexander Repnikov <a.repnikov@visuality.pl>
2026-03-04 23:02:04 +0100

    App configuration

--- app/src/main/java/com/example/hotwirenativeworkshop/KanbanBoardApplication.kt
+++ app/src/main/java/com/example/hotwirenativeworkshop/KanbanBoardApplication.kt
@@ -1,18 +1,29 @@
 package com.example.hotwirenativeworkshop
 
 import android.app.Application
+import dev.hotwire.core.BuildConfig
+import dev.hotwire.core.bridge.KotlinXJsonConverter
 import dev.hotwire.core.config.Hotwire
 import dev.hotwire.core.turbo.config.PathConfiguration
 
 class KanbanBoardApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        configureApp()
+    }
 
+    private fun configureApp() {
         Hotwire.loadPathConfiguration(
             context = this,
             location = PathConfiguration.Location(
                 assetFilePath = "json/path-configuration.json"
             )
         )
+
+        // Set configuration options
+        Hotwire.config.debugLoggingEnabled = BuildConfig.DEBUG
+        Hotwire.config.webViewDebuggingEnabled = BuildConfig.DEBUG
+        Hotwire.config.jsonConverter = KotlinXJsonConverter()
+        Hotwire.config.applicationUserAgentPrefix = "Kanban Boards;"
     }
 }
