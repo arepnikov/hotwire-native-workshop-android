deeba75f54ccd5c0f68030a37adbd7fee5ed4199
Alexander Repnikov <aleksander.repnikov@gmail.com>
2026-03-08 00:32:40 +0100

    Switch Environment from Remote to Local

--- app/src/main/java/com/example/hotwirenativeworkshop/KanbanBoard.kt
+++ app/src/main/java/com/example/hotwirenativeworkshop/KanbanBoard.kt
@@ -2,7 +2,7 @@ package com.example.hotwirenativeworkshop
 
 object KanbanBoard {
     // Update this to choose which demo is run
-    val current: Environment = Environment.Remote
+    val current: Environment = Environment.Local
 
     enum class Environment(val url: String) {
         Remote("https://hotwire-native-demo.dev"),
